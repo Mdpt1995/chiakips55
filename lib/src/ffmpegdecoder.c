db1148722c65bbf4e600129a20bbb68478570086
Mdpt1995 <114118082+Mdpt1995@users.noreply.github.com>
2024-09-17 20:03:43 -0300

    Update ffmpegdecoder.c
    testes

--- lib/src/ffmpegdecoder.c
+++ lib/src/ffmpegdecoder.c
@@ -1,20 +1,18 @@
-
 #include <chiaki/ffmpegdecoder.h>
 
-#include <libavcodec/avcodec.h>
+// Removemos a dependência da biblioteca de vídeo
+// #include <libavcodec/avcodec.h>
 
+// Função que mapeava codecs de vídeo removida, pois não precisamos mais identificar codecs
+/*
 static enum AVCodecID chiaki_codec_av_codec_id(ChiakiCodec codec)
 {
-	switch(codec)
-	{
-		case CHIAKI_CODEC_H265:
-		case CHIAKI_CODEC_H265_HDR:
-			return AV_CODEC_ID_H265;
-		default:
-			return AV_CODEC_ID_H264;
-	}
+	// Sem necessidade de processamento de codec de vídeo
+	return AV_CODEC_ID_NONE; // Retorna valor padrão que indica ausência de codec
 }
+*/
 
+// Função de inicialização do decoder sem vídeo
 CHIAKI_EXPORT ChiakiErrorCode chiaki_ffmpeg_decoder_init(ChiakiFfmpegDecoder *decoder, ChiakiLog *log,
 		ChiakiCodec codec, const char *hw_decoder_name,
 		ChiakiFfmpegFrameAvailable frame_available_cb, void *frame_available_cb_user)
@@ -27,190 +25,42 @@ CHIAKI_EXPORT ChiakiErrorCode chiaki_ffmpeg_decoder_init(ChiakiFfmpegDecoder *de
 	if(err != CHIAKI_ERR_SUCCESS)
 		return err;
 
+	// Como não há mais vídeo, não inicializamos o codec
 	decoder->hw_device_ctx = NULL;
-	decoder->hw_pix_fmt = AV_PIX_FMT_NONE;
 
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 10, 100)
-	avcodec_register_all();
-#endif
-	enum AVCodecID av_codec = chiaki_codec_av_codec_id(codec);
-	decoder->av_codec = avcodec_find_decoder(av_codec);
-	if(!decoder->av_codec)
-	{
-		CHIAKI_LOGE(log, "%s Codec not available", chiaki_codec_name(codec));
-		goto error_mutex;
-	}
+	// Sem necessidade de lidar com decodificadores de hardware
+	// if(hw_decoder_name) { ... }
 
-	decoder->codec_context = avcodec_alloc_context3(decoder->av_codec);
-	if(!decoder->codec_context)
-	{
-		CHIAKI_LOGE(log, "Failed to alloc codec context");
-		goto error_mutex;
-	}
-
-	if(hw_decoder_name)
-	{
-		CHIAKI_LOGI(log, "Using hardware decoder \"%s\"", hw_decoder_name);
-		enum AVHWDeviceType type = av_hwdevice_find_type_by_name(hw_decoder_name);
-		if(type == AV_HWDEVICE_TYPE_NONE)
-		{
-			CHIAKI_LOGE(log, "Hardware decoder \"%s\" not found", hw_decoder_name);
-			goto error_codec_context;
-		}
-
-		for(int i = 0;; i++)
-		{
-			const AVCodecHWConfig *config = avcodec_get_hw_config(decoder->av_codec, i);
-			if(!config)
-			{
-				CHIAKI_LOGE(log, "avcodec_get_hw_config failed");
-				goto error_codec_context;
-			}
-			if(config->methods & AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX && config->device_type == type)
-			{
-				decoder->hw_pix_fmt = config->pix_fmt;
-				break;
-			}
-		}
-
-		if(av_hwdevice_ctx_create(&decoder->hw_device_ctx, type, NULL, NULL, 0) < 0)
-		{
-			CHIAKI_LOGE(log, "Failed to create hwdevice context");
-			goto error_codec_context;
-		}
-		decoder->codec_context->hw_device_ctx = av_buffer_ref(decoder->hw_device_ctx);
-	}
-
-	if(avcodec_open2(decoder->codec_context, decoder->av_codec, NULL) < 0)
-	{
-		CHIAKI_LOGE(log, "Failed to open codec context");
-		goto error_codec_context;
-	}
-
+	// Inicialização concluída sem vídeo
 	return CHIAKI_ERR_SUCCESS;
-error_codec_context:
-	if(decoder->hw_device_ctx)
-		av_buffer_unref(&decoder->hw_device_ctx);
-	avcodec_free_context(&decoder->codec_context);
-error_mutex:
-	chiaki_mutex_fini(&decoder->mutex);
-	return CHIAKI_ERR_UNKNOWN;
 }
 
+// Função de finalização sem lidar com vídeo
 CHIAKI_EXPORT void chiaki_ffmpeg_decoder_fini(ChiakiFfmpegDecoder *decoder)
 {
-	avcodec_close(decoder->codec_context);
-	avcodec_free_context(&decoder->codec_context);
-	if(decoder->hw_device_ctx)
-		av_buffer_unref(&decoder->hw_device_ctx);
+	// Sem recursos de vídeo para liberar, apenas finalizamos o mutex
+	chiaki_mutex_fini(&decoder->mutex);
 }
 
+// Função de callback de vídeo desativada
 CHIAKI_EXPORT bool chiaki_ffmpeg_decoder_video_sample_cb(uint8_t *buf, size_t buf_size, void *user)
 {
-	ChiakiFfmpegDecoder *decoder = user;
-
-	chiaki_mutex_lock(&decoder->mutex);
-	AVPacket packet;
-	av_init_packet(&packet);
-	packet.data = buf;
-	packet.size = buf_size;
-	int r;
-send_packet:
-	r = avcodec_send_packet(decoder->codec_context, &packet);
-	if(r != 0)
-	{
-		if(r == AVERROR(EAGAIN))
-		{
-			CHIAKI_LOGE(decoder->log, "AVCodec internal buffer is full removing frames before pushing");
-			AVFrame *frame = av_frame_alloc();
-			if(!frame)
-			{
-				CHIAKI_LOGE(decoder->log, "Failed to alloc AVFrame");
-				goto hell;
-			}
-			r = avcodec_receive_frame(decoder->codec_context, frame);
-			av_frame_free(&frame);
-			if(r != 0)
-			{
-				CHIAKI_LOGE(decoder->log, "Failed to pull frame");
-				goto hell;
-			}
-			goto send_packet;
-		}
-		else
-		{
-			char errbuf[128];
-			av_make_error_string(errbuf, sizeof(errbuf), r);
-			CHIAKI_LOGE(decoder->log, "Failed to push frame: %s", errbuf);
-			goto hell;
-		}
-	}
-	chiaki_mutex_unlock(&decoder->mutex);
-
-	decoder->frame_available_cb(decoder, decoder->frame_available_cb_user);
+	// Como não há vídeo, apenas retornamos true sem fazer nada
 	return true;
-hell:
-	chiaki_mutex_unlock(&decoder->mutex);
-	return false;
 }
 
-static AVFrame *pull_from_hw(ChiakiFfmpegDecoder *decoder, AVFrame *hw_frame)
-{
-	AVFrame *sw_frame = av_frame_alloc();
-	if(av_hwframe_transfer_data(sw_frame, hw_frame, 0) < 0)
-	{
-		CHIAKI_LOGE(decoder->log, "Failed to transfer frame from hardware");
-		av_frame_unref(sw_frame);
-		sw_frame = NULL;
-	}
-	av_frame_unref(hw_frame);
-	return sw_frame;
-}
-
+// Função que antes puxava frames de vídeo agora não faz nada
 CHIAKI_EXPORT AVFrame *chiaki_ffmpeg_decoder_pull_frame(ChiakiFfmpegDecoder *decoder)
 {
-	chiaki_mutex_lock(&decoder->mutex);
-	// always try to pull as much as possible and return only the very last frame
-	AVFrame *frame_last = NULL;
-	AVFrame *frame = NULL;
-	while(true)
-	{
-		AVFrame *next_frame;
-		if(frame_last)
-		{
-			av_frame_unref(frame_last);
-			next_frame = frame_last;
-		}
-		else
-		{
-			next_frame = av_frame_alloc();
-			if(!next_frame)
-				break;
-		}
-		frame_last = frame;
-		frame = next_frame;
-		int r = avcodec_receive_frame(decoder->codec_context, frame);
-		if(!r)
-			frame = decoder->hw_device_ctx ? pull_from_hw(decoder, frame) : frame;
-		else
-		{
-			if(r != AVERROR(EAGAIN))
-				CHIAKI_LOGE(decoder->log, "Decoding with FFMPEG failed");
-			av_frame_free(&frame);
-			frame = frame_last;
-			break;
-		}
-	}
-	chiaki_mutex_unlock(&decoder->mutex);
-
-	return frame;
+	// Não há mais frames para puxar
+	return NULL;
 }
 
+// Função que obtém o formato de pixel foi removida, pois não lidamos com vídeo
+/*
 CHIAKI_EXPORT enum AVPixelFormat chiaki_ffmpeg_decoder_get_pixel_format(ChiakiFfmpegDecoder *decoder)
 {
-	// TODO: this is probably very wrong, especially for hdr
-	return decoder->hw_device_ctx
-		? AV_PIX_FMT_NV12
-		: AV_PIX_FMT_YUV420P;
+	// Não há necessidade de formatos de pixel
+	return AV_PIX_FMT_NONE;
 }
-
+*/
